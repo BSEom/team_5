93b23a49dd1a928f8b29f0c48e4df11c06d74176
hyebinrk/python <gpqls3698@naver.com>
2025-02-18 12:13:08 +0900

    계산_3

--- date/계산_3.py
+++ date/계산_3.py
@@ -1,6 +1,11 @@
 import datetime
-days = ['월', '화', '수', '목', '금', '토', '일']
-p_day = datetime.date(2025,5,8)
-today = datetime.datetime.now().date()
-diff3 = p_day - today
-print("어버이날까지 " + str(diff3.days) + "일 남았습니다. 어버이날은 " + days[p_day.weekday()] + "요일 입니다.")
+
+def pday():
+    days = ['월', '화', '수', '목', '금', '토', '일']
+        
+    p_day = datetime.date(2025,5,8)
+    today = datetime.datetime.now().date()
+
+    diff3 = p_day - today
+
+    print("어버이날까지 " + str(diff3.days) + "일 남았습니다. 어버이날은 " + days[p_day.weekday()] + "요일 입니다.")
