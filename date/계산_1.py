c82b300a4a79a4015e6903cb06c83eb38086d953
JeongHui-seong <jgmltjd99@naver.com>
2025-02-18 12:06:51 +0900

    계산1 수정

--- date/계산_1.py
+++ date/계산_1.py
@@ -1,24 +1,26 @@
 import datetime
 
-inp = input("생일 언제임 (월, 일만 입력해주고 숫자가 한자리이면 앞에 0붙여주기 안그러면 큰일남 귀찮게 해서 ㅈㅅ)")
-inp = inp.replace('월','').replace('일','').replace('-','').replace(' ', '')
-
-if len(inp) != 4:
-    print("큰일났음 다시 입력해주삼")
+def birthday():
+    
     inp = input("생일 언제임 (월, 일만 입력해주고 숫자가 한자리이면 앞에 0붙여주기 안그러면 큰일남 귀찮게 해서 ㅈㅅ)")
-else:
-    month = int(inp[:2].replace('0', ''))
-    date = int(inp[2:].replace('0', ''))
-    birthday = datetime.date(2025,month,date)
-    today = datetime.datetime.now().date()
+    inp = inp.replace('월','').replace('일','').replace('-','').replace(' ', '')
     
-    birthday_diff = today - birthday
-    days = ['월','화','수','목','금','토','일']
-    birthday_days = birthday.weekday()
-    
-    if birthday_diff.days < 0:
-        print(f"생일 {days[birthday_days]}요일이네 {abs(birthday_diff.days)}일 남았다 미리 ㅊㅊㅊㅊ")
-    elif birthday_diff.days > 0:
-        print(f"생일 {days[birthday_days]}요일이었네 {abs(birthday_diff.days)}일 지났네")
+    if len(inp) != 4:
+        print("큰일났음 다시 입력해주삼")
+        inp = input("생일 언제임 (월, 일만 입력해주고 숫자가 한자리이면 앞에 0붙여주기 안그러면 큰일남 귀찮게 해서 ㅈㅅ)")
     else:
-        print("오늘 생일임? ㅊㅊㅊㅊ")
+        month = int(inp[:2].replace('0', ''))
+        date = int(inp[2:].replace('0', ''))
+        birthday = datetime.date(2025,month,date)
+        today = datetime.datetime.now().date()
+        
+        birthday_diff = today - birthday
+        days = ['월','화','수','목','금','토','일']
+        birthday_days = birthday.weekday()
+        
+        if birthday_diff.days < 0:
+            print(f"생일 {days[birthday_days]}요일이네 {abs(birthday_diff.days)}일 남았다 미리 ㅊㅊㅊㅊ")
+        elif birthday_diff.days > 0:
+            print(f"생일 {days[birthday_days]}요일이었네 {abs(birthday_diff.days)}일 지났네")
+        else:
+            print("오늘 생일임? ㅊㅊㅊㅊ")
