393879a236eba01130628730cb40ec41863ed336
python_pyb <dbqls3698@naver.com>
2025-02-27 09:18:38 +0900

    서울 3호선

---
+++ SeoulSubway/seoul3.py
@@ -0,0 +1,25 @@
+def line3():
+    import pandas as pd
+    import matplotlib
+    import matplotlib.pyplot as plt
+    import matplotlib.font_manager as fm
+    import squarify
+    
+    file_path = "./ref/서울시지하철호선별역별승하차인원정보.csv"
+    df = pd.read_csv(file_path,encoding="EUC-KR")
+    df['total'] = df['승차총승객수'] + df['하차총승객수']
+    total_data = df[['호선명','역명','total']]
+    data = total_data[total_data['호선명']=='3호선']
+    mdata = data.groupby(by='역명').sum().reset_index()
+    tdata = mdata[['역명','total']]
+    
+    d2_path = "./ref/D2Coding-Ver1.3.2-20180524.ttf"
+    fm.fontManager.addfont(d2_path)
+    plt.rcParams["font.family"] = "D2Coding"
+    
+    plt.figure(figsize=(25,7))
+    squarify.plot(sizes=tdata['total'], label = [f"{x[0]}({x[1]}명)" for x in zip(tdata['역명'],tdata['total'])],alpha=0.5)
+    plt.rcParams['font.size'] = 10
+    plt.gca().invert_yaxis()
+    plt.axis("off")
+    plt.show()
